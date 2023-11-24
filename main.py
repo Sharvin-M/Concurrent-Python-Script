84ed3decdf8e28c5f6dd3bcef5c8f6a097a91bbd
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-23 20:27:57 -0800

    using vim motions to refactor code

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ import pyautogui
 haystack_img = cv.imread("queuePop.jpg")
 needle_img = cv.imread("acceptButton.jpg")
 
+# method that locates needle in haystack
+result = cv.matchTemplate(haystack_img, needle_img,  cv.TM_CCOEFF_NORMED) 
 
-result = cv.matchTemplate(haystack_img, needle_img,  cv.TM_CCOEFF_NORMED) # method that locates needle in haystack
 
-
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) # returns the whitest and darkest pixels and their position
-# maxVal = confidence 
-# maxLoc = Location
-# common practice to set a threshold for confidence 
-
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) 
+# maxVal = confidence and maxLoc = Location
+# common practice to set a threshold for confidence
+ 

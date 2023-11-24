d529da39dfbd99bd0b5e851a4c5db49bf5abefee
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-23 18:09:22 -0800

    trying to fix error with image reading

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 import numpy as np
 
+
+
 haystack_img = cv.imread("queuePop.jpg", cv.IMREAD_UNCHANGED)
 needle_img = cv.imread("acceptButton.jpg", cv.IMREAD_UNCHANGED)
 
 
-result = cv.matchTemplate(haystack_img, needle_img,  cv.TM_SQDIFF_NORMED) # method that locates needle in haystack
+result = cv.matchTemplate(haystack_img, needle_img,  cv.TM_CCOEFF_NORMED) # method that locates needle in haystack
 
 
-cv.imshow('Result', result) # shows script
+cv.imshow('Result', result) # script
 cv.waitKey() # pauses script

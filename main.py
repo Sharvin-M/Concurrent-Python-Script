1dd04a4154329dca2420106e66c675954afc1f66
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-23 18:25:45 -0800

    produces confidence and location of needle image

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ needle_img = cv.imread("acceptButton.jpg")
 result = cv.matchTemplate(haystack_img, needle_img,  cv.TM_CCOEFF_NORMED) # method that locates needle in haystack
 
 
-cv.imshow('Result', result) # script
-cv.waitKey() # pauses script
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) # returns the whitest and darkest pixels and their position
+# maxVal = confidence 
+# maxLoc = Location
+# common practice to set a threshold for confidence 
+
+print(max_val)
+print(max_loc)

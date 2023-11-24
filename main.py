69691ee3fb940cce9647993fc7b0438d5ef0034a
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-23 18:39:18 -0800

    creat func to draw rectangle, then find mp, then click

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-
+import pyautogui
 
 
 haystack_img = cv.imread("queuePop.jpg")
@@ -15,5 +15,3 @@ min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) # returns the whitest 
 # maxLoc = Location
 # common practice to set a threshold for confidence 
 
-print(max_val)
-print(max_loc)

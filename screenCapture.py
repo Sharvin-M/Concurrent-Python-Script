ae3cc72c5c8bba54e876abdd2027329e97bbe742
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-11-23 23:11:09 -0800

    made a mistake trying to use pywin to screen capture

--- screenCapture.py
+++ screenCapture.py
@@ -1,10 +1,43 @@
 import cv2 as cv
 import numpy as np
 import pyautogui
+import cv2 as cv
+import numpy as np
+import pygetwindow as gw
+import win32gui
+import win32ui
 
-# Set the screen capture dimensions
-screen_width, screen_height = pyautogui.size()
-screen_capture_region = (0, 0, screen_width, screen_height)
+# Function to capture the entire screen
+def capture_screen():
+    # Get the screen dimensions
+    screen_width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
+    screen_height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
+
+    # Create a device context
+    hdesktop = win32gui.GetDesktopWindow()
+    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+
+    # Create a compatible bitmap
+    compatible_bitmap = win32ui.CreateBitmap()
+    compatible_bitmap.CreateCompatibleBitmap(img_dc, screen_width, screen_height)
+
+    # Select the compatible bitmap into the device context
+    img_dc.SelectObject(compatible_bitmap)
+
+    # Copy the screen into the compatible bitmap
+    img_dc.BitBlt((0, 0), (screen_width, screen_height), img_dc, (0, 0), win32con.SRCCOPY)
+
+    # Convert the bitmap to an image
+    bmp_info = compatible_bitmap.GetInfo()
+    bmp_str = compatible_bitmap.GetBitmapBits(True)
+    screen_image = np.frombuffer(bmp_str, dtype='uint8').reshape((bmp_info['bmHeight'], bmp_info['bmWidth'], 4))
+
+    # Release the device context
+    img_dc.DeleteDC()
+    win32gui.ReleaseDC(hdesktop, desktop_dc)
+
+    return screen_image
 
 # Create a window to display the captured screen
 cv.namedWindow('Screen Capture', cv.WINDOW_NORMAL)
@@ -12,15 +45,14 @@ cv.namedWindow('Screen Capture', cv.WINDOW_NORMAL)
 # Continuously capture the screen and display it
 while True:
     # Capture the screen image
-    screen_image = pyautogui.screenshot(region=screen_capture_region)
-    
+    screen_image = capture_screen()
+
     # Convert the image to OpenCV format
-    frame = np.array(screen_image)
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    
+    frame = cv.cvtColor(screen_image, cv.COLOR_BGRA2RGB)
+
     # Display the captured screen image
     cv.imshow('Screen Capture', frame)
-    
+
     # Break the loop if 'q' is pressed
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()

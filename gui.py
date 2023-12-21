0f96fad8c919e35b9870b2af785bafa7872acc9f
Sharvin-M <smanjrekar2@ucmerced.edu>
2023-12-20 16:04:58 -0800

    fix: close button functionality

--- gui.py
+++ gui.py
@@ -1,18 +1,23 @@
 import customtkinter
 from auto_accept import accept
 
+
 def button_callback():
     accept()  # Call the accept() function from the other module
 
-# def close_callback():
-    # app.destroy()
+
+def close_callback():
+    app.destroy()
 
 
 app = customtkinter.CTk()
 app.title("Auto Accept")
 app.geometry("400x150")
 
-button = customtkinter.CTkButton(app, text="In Queue", command=button_callback)
-button.grid(row=200, column=500, padx=20, pady=20)
+runButton = customtkinter.CTkButton(app, text="Queue", command=button_callback)
+runButton.grid(row=200, column=500, padx=20, pady=20)
+
+closeButton = customtkinter.CTkButton(app, text="Close", command=close_callback)
+closeButton.grid(row=200, column=550, padx=20, pady=20)
 
 app.mainloop()
